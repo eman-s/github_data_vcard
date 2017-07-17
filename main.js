9f1cf186215cb17c40d1de68a24eda2f197e9d44
Emmanuel Salicrup <emmanuel_salicrup@live.com>
2017-07-17 15:27:47 -0400

    getting set up

---
+++ main.js
@@ -0,0 +1,24 @@
+let h1 = document.querySelector(".name");
+let ul = document.querySelector(".basics")
+let story = document.querySelector(".story")
+let p = document.createElement("p");
+
+story.appendChild(p);
+
+
+let req = new XMLHttpRequest();
+req.open("GET", "https://api.github.com/users/eman-s");
+
+req.addEventListener("load", function(){
+  let obj = JSON.parse(req.response);
+  console.log(obj.name);
+    h1.innerHTML = `${obj.name}`;
+
+    ul.innerHTML = `Name: ${obj.name} <br> Github URL: ${obj.html_url} <br> Email: ${obj.email} <br> Company: ${obj.company} <br> Website: ${obj.blog} `;
+
+    p.innerHTML = `${obj.bio}`;
+
+
+});
+
+req.send();
